1015d6071126c112894a26b8d61ecaf6bbae8c30
jkasper <jkasper@student.42heilbronn.de>
2021-11-23 23:25:34 +0100

    finished experimental version

--- parser/tree_builder.c
+++ parser/tree_builder.c
@@ -6,7 +6,7 @@
 /*   By: jkasper <jkasper@student.42Heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/22 17:35:59 by jkasper           #+#    #+#             */
-/*   Updated: 2021/11/23 19:24:16 by jkasper          ###   ########.fr       */
+/*   Updated: 2021/11/23 23:13:37 by jkasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,107 @@
 #include "minis.h"
 #include "parser.h"
 
-t_bin	*add_com(t_token *tok);
-int		par_check_com(t_node *node);
+int	count_words(t_node *node)
+{
+	int	counter;
+
+	counter = 0;
+	while (node == NULL && node->content->type == WORD)
+		counter++;
+	return (counter);
+}
+
+node	*add_io(t_bin *tree, t_node *node)
+{
+	if (tree->io == NULL)
+		tree->io = ft_calloc(1, sizeof(t_io));
+	if (node->content->type == HERE_DOC || node->content->type == IRD)
+	{
+		tree->io->i_mode = node->content->type - HERE_DOC + 1;
+		if (tree->io->input == NULL)
+			tree->io->input = ft_calloc(1, sizeof(char *));
+		else
+			tree->io->input = ft_realloc_charpp(&tree->io->input, \
+			ft_char_arr_len(tree->io->input) + 2);
+		tree->io->input[ft_char_arr_len(tree->io->input)] = \
+		ft_strdup(node->next->content->string);
+	}
+	else
+	{
+		tree->io->o_mode = node->content->type - HERE_DOC + 1;
+		if (tree->io->output == NULL)
+			tree->io->output = ft_calloc(1, sizeof(char *));
+		else
+			tree->io->output = ft_realloc_charpp(&tree->io->output, \
+			ft_char_arr_len(tree->io->output) + 2);
+		tree->io->output[ft_char_arr_len(tree->io->output)] = \
+		ft_strdup(node->next->content->string);
+	}
+	return (node->next->next);
+}
+
+t_node	*add_words(t_simple_com *command, t_node *node)
+{
+	int	n_words;
+	int	i;
+
+	n_words = count_words(node);
+	command->number_arguments += n_words;
+	if (command->number_arguments == n_words)
+		command->arguments = calloc(n_words + 1, sizeof(char *));
+	else
+		command->arguments = ft_realloc_charpp(&command->arguments, \
+		command->number_arguments);
+	i = command->number_arguments - n_words;
+	while (i < n_words)
+	{
+		command->arguments[i] = ft_strdup(node->content->string);
+		node = node->next;
+		i++;
+	}
+	return (node);
+}
+
+t_bin	*add_operator(t_e_op en_op, t_bin *parent)
+{
+	t_bin	*ret;
+
+	ret = ft_calloc(1, sizeof(t_bin));
+	ret->parent = parent;
+	ret->e_op = en_op;
+	ret->command = ft_calloc(1, sizeof(t_simple_com));
+	ret->child = NULL;
+	return (ret);
+}
+
+t_bin	*add_com(t_node **ori_node, t_bin *parent)
+{
+	t_bin	*ret;
+	t_node	*node;
+	t_e_op	token;
+
+	node = *ori_node;
+	token = node->content->type;
+	if (token == OPAR)
+		return (NULL);
+	if (token < OPAR)
+	{
+		ret = add_operator(token, parent);
+		node = node->next;
+		token = node->content->type;
+	}
+	while (node != NULL)
+	{
+		if (token > CPAR && token < QUOTE)
+			node = add_io(ret, node);
+		if (token < OPAR)
+			break ;
+		node = add_words(ret->command, node);
+		node = node->next;
+		node->content->type;
+	}
+	return (ret);
+}
 
 t_node	*jump_token(t_node *node, int cmp)
 {
