f770ffee7fbf28e812847b04ae5d93bee91096bf
jkasper <jkasper@student.42heilbronn.de>
2021-12-04 21:17:32 +0100

    updated cd.c

--- builtin/cd.c
+++ builtin/cd.c
@@ -6,7 +6,7 @@
 /*   By: jkasper <jkasper@student.42Heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/16 13:53:37 by jkasper           #+#    #+#             */
-/*   Updated: 2021/11/16 15:49:28 by jkasper          ###   ########.fr       */
+/*   Updated: 2021/12/04 21:17:17 by jkasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,11 @@ int	cd_main(char **input, t_data *data)
 	int		i;
 	char	*path;
 
-	if (chdir(input[1]) == -1)
+	if (!ft_strncmp(input[1], "-\0", 2))
+		chdir(getenv("OLDPWD"));
+	else if (input[1] == NULL)
+		chdir(getenv("HOME"));
+	else if (chdir(input[1]) == -1)
 	{
 		ft_putstring_fd("No such file or directory: ");
 		ft_putstring_fd(input[1]);
