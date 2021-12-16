a9d8370a2985c0b18c1d65cd965209cad5747603
jkasper <jkasper@student.42heilbronn.de>
2021-12-16 16:55:52 +0100

    added list_to_array

--- global_utils.c
+++ global_utils.c
@@ -6,7 +6,7 @@
 /*   By: jkasper <jkasper@student.42Heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 19:32:03 by jkasper           #+#    #+#             */
-/*   Updated: 2021/12/16 01:01:46 by jkasper          ###   ########.fr       */
+/*   Updated: 2021/12/16 16:55:31 by jkasper          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,4 +36,34 @@ char	*mini_getenv(t_data *data, char *var_name)
 	if (temp == NULL)
 		return (NULL);
 	return (&((char *)temp->content)[start]);
+}
+
+char	**list_to_array(t_node *node)
+{
+	char	**ret;
+	int		i;
+
+	ret = ft_calloc(ft_s_node_iter(node) + 1, sizeof(char *));
+	i = 0;
+	while (node != NULL)
+	{
+		ret[i] = ft_strdup(((char *)node->content));
+		node = node->next;
+		i++;
+	}
+	ret[i] = NULL;
+	return (ret);
+}
+
+int	ft_s_node_iter(struct s_node *head)
+{
+	int	i;
+
+	i = 0;
+	while (head != NULL)
+	{
+		head = head->next;
+		i++;
+	}
+	return (i);
 }
